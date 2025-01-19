1d1453956d4ad9960ca6d656423d0d4b6e24ee01
Beshoy William <165554296+BeshoyWill@users.noreply.github.com>
2025-01-19 21:02:38 +0200

    Update Program.cs

--- SessionTask/Program.cs
+++ SessionTask/Program.cs
@@ -1,10 +1,58 @@
-﻿namespace SessionTask
+using SessionTask.interfaces;
+using static System.Console;
+
+namespace SessionTask
 {
     internal class Program
     {
+        private static int orderId;
+        private static decimal orderAmount;
+        private static string customerName;
+
+
+
+        static IOrderProcessor GetOrderProcessor(string orderType)
+        {
+            return orderType.ToLower() switch
+            {
+                "online" => new OnlineOrderProcessor(),
+                "in-store" => new InStoreOrderProcessor(),
+                _ => throw new ArgumentException("Invalid order type. Please enter 'Online' or 'In-Store'.")
+            };
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            try
+            {
+                Console.WriteLine("Enter Order ID: ");
+                int OrderId = int.Parse(ReadLine());
+
+                Console.WriteLine("Enter Customer Name: ");
+                string CustomerName = ReadLine();
+
+                Console.WriteLine("Enter Order Amount: ");
+                decimal OrderAmount = decimal.Parse(ReadLine());
+
+
+                Console.Write("Enter Order Type (Online/In-Store): ");
+                string orderType = Console.ReadLine();
+
+                Order order = new Order
+                {
+                    OrderId = orderId,
+                    CustomerName = customerName,
+                    OrderAmount = orderAmount,
+                    OrderProcessor = GetOrderProcessor(orderType)
+                };
+
+                order.OrderProcessor.ProcessOrder(order);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
         }
     }
 }
